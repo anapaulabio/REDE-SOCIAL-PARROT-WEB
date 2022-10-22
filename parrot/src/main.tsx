74ffd52eda64e446856edbdf33aa6fe87076dde9
danilordev <danilordev@gmail.com>
2022-10-22 16:18:29 -0300

    instalando bootstrap

--- parrot/src/main.tsx
+++ parrot/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <h1>Hello world</h1>
+    <a href='#' className='btn btn-primary'>botao bootstrap</a>
   </React.StrictMode>
 )
