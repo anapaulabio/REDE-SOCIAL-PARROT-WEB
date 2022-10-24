85b3b11b85efb45202356933c12db51f28b41e6a
GuilhermeRibeiro01 <guilhermeribeirosantos02@outlook.com>
2022-10-24 12:45:31 -0300

    Navbar

--- parrot/src/main.tsx
+++ parrot/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Navbar from './components/Navbar'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <h1>Hello world</h1>
+    <h1>hello world</h1>
   </React.StrictMode>
 )
