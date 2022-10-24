8edb60a5b9f71920f5ffd675a11f85a19cd7e457
danilordev <danilordev@gmail.com>
2022-10-24 20:59:35 -0300

    atualizaçao
    correçao

--- parrot/src/pages/Feed.tsx
+++ parrot/src/pages/Feed.tsx
@@ -8,33 +8,33 @@ function Feed() {
     <div>
       <Navbar />
 
-      <div className="row text-center">
-        {/* ******** */}
-        <div className="col-3 border border-dark"></div>
-        {/* ******** */}
-        <div className="col-md-6 d-flex border border-dark p-4">
-          <div className="p-3">
-            <IconImg src={icon} alt="icon" />
-          </div>
+      <div className="h-100 row text-center">
+      
+        <div className="p-0 h-100 col-md-6 offset-md-3 border border-dark">
+          <div className="d-flex border-bottom border border-dark  p-4">
+            <div className="px-3">
+              <IconImg src={icon} alt="icon" />
+            </div>
 
-          <div className="col-10 p-3 ">
-            <textarea
-              className="form-control"
-              placeholder="Leave a comment here"
-            ></textarea>
+            <div className="col-10 p-2 ">
+              <textarea
+                className="form-control border-success"
+                placeholder="Leave a comment here"
+              ></textarea>
 
-            <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-              <button
-                className="btn me-md-2 m-4 border border-dark"
-                type="button"
-              >
-                Publicar
-              </button>
+              <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button
+                  className="btn me-md-2 m-4 border border-dark"
+                  type="button"
+                >
+                  Publicar
+                </button>
+              </div>
             </div>
           </div>
         </div>
         {/* ******** */}
-        <div className="col-3 border border-dark"></div>
+        {/* <div className="col-3 border border-dark"></div> */}
         {/* ******** */}
       </div>
     </div>
