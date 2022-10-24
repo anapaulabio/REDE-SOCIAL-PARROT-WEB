0aa32e81f20221f3200ed600a2523aecbf6cc335
danilordev <danilordev@gmail.com>
2022-10-24 20:06:09 -0300

    atualizaçao

--- parrot/src/main.tsx
+++ parrot/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './components/Navbar'
 import './index.css'
+import Feed from './pages/Feed'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <h1>hello world</h1>
+    <Feed />
   </React.StrictMode>
 )
--- parrot/src/pages/Feed.tsx
+++ parrot/src/pages/Feed.tsx
@@ -1,7 +1,44 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "../components/Navbar";
+import { IconImg } from "./feed.style";
+import icon from "../../img/icon.png";
 
+function Feed() {
+  return (
+    <div>
+      <Navbar />
 
-function Feed(){
+      <div className="row text-center">
+        {/* ******** */}
+        <div className="col-3 border border-dark"></div>
+        {/* ******** */}
+        <div className="col-md-6 d-flex border border-dark p-4">
+          <div className="p-3">
+            <IconImg src={icon} alt="icon" />
+          </div>
 
+          <div className="col-10 p-3 ">
+            <textarea
+              className="form-control"
+              placeholder="Leave a comment here"
+            ></textarea>
+
+            <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+              <button
+                className="btn me-md-2 m-4 border border-dark"
+                type="button"
+              >
+                Publicar
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* ******** */}
+        <div className="col-3 border border-dark"></div>
+        {/* ******** */}
+      </div>
+    </div>
+  );
 }
 
 export default Feed;
